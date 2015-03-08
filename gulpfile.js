8154c3168f7418a66e1db7f3a994f32ba8fe30b9
nezz <allstars@gmail.com>
2015-03-08 16:20:33 +0900

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 var gulp = require('gulp'),
 	browserSync = require('browser-sync');
 
+gulp.task('html', function() {
+	gulp.src('app/*.html')
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('js', function() {
+	gulp.src('app/js/*.js')
+		.pipe(gulp.dest('dist/js/'));
+	});
+
+gulp.task('css', function() {
+	gulp.src('app/css/*.css')
+		.pipe(gulp.dest('dist/css/'));
+	});
+
 gulp.task('serve', function() {
 	browserSync({
 		server: {
-			baseDir: './app'
+			baseDir: './dist'
 		}
 	});
 
-	gulp.watch('app/*.html').on('change', browserSync.reload);
-	gulp.watch(['app/js/*.js', 'app/css/*.css'], browserSync.reload);
+	gulp.watch('app/*.html').on('change', function() {
+		gulp.run('html');
+		browserSync.reload;
+	});
+	gulp.watch(['app/js/*.js', 'app/css/*.css'], function() {
+		gulp.run('js');
+		gulp.run('css');
+		browserSync.reload;
+	});
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['html', 'js', 'css', 'serve']);
